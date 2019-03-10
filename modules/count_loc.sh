d7cc8c7a0657f0f0c027539fdc6a9cc651ba8f01
David Ke Hong <hongkedavid@gmail.com>
2019-03-10 17:05:53 -0400

    Update count_loc.sh

--- modules/count_loc.sh
+++ modules/count_loc.sh
@@ -1,1 +1,1 @@
-c=0; for f in $(find . -type f -name *.cc | grep -v "\_test\."); do n=$(cat $f | wc -l); c=$(($c+$n)); done; for f in $(find . -type f -name *.h | grep -v "\_test\."); do n=$(cat $f | wc -l); c=$(($c+$n)); done; echo $c
+c=0; for f in $(find . -type f -name *.cpp | grep -v "\_test\."); do n=$(cat $f | wc -l); c=$(($c+$n)); done; for f in $(find . -type f -name *.cc | grep -v "\_test\."); do n=$(cat $f | wc -l); c=$(($c+$n)); done; for f in $(find . -type f -name *.h | grep -v "\_test\."); do n=$(cat $f | wc -l); c=$(($c+$n)); done; echo $c
