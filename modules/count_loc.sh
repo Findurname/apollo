cfc4ab997e8f8c96eb770f5b4ab8ff80498c8bdd
David Ke Hong <hongkedavid@gmail.com>
2019-03-10 16:34:58 -0400

    Create count_loc.sh

---
+++ modules/count_loc.sh
@@ -0,0 +1,1 @@
+c=0; for f in $(find . -type f -name *.cc | grep -v "\_test\."); do n=$(cat $f | wc -l); c=$(($c+$n)); done; for f in $(find . -type f -name *.h | grep -v "\_test\."); do n=$(cat $f | wc -l); c=$(($c+$n)); done; echo $c
